2fe3d3e6b93166a4297bf65bf711bc1984a2e2cc
Eplistical <gaohan@sas.upenn.edu>
2018-06-29 16:18:59 -0400

    complete showsurf to show unbroadened/broadened surfaces

--- showsurf.cpp
+++ showsurf.cpp
@@ -8,53 +8,65 @@
 #include "misc/vector.hpp"
 #include "misc/ioer.hpp"
 #include "misc/timer.hpp"
+#include "misc/fermi.hpp"
 #include "misc/randomer.hpp"
 
 #include "config.hpp"
 #include "loadinit.hpp"
 #include "argparse.hpp"
-#include "hamiltonian_1d/impurity_near_uniform_band_hamiltonian.hpp"
-#include "particle_1d/iesh_particle_1d.hpp"
-
-using hamiltonian_t = ImpurityNearUniformBandHamiltonian_1D<potential_t>;
-using ptcl_t = IESH_Particle_1D<hamiltonian_t>;
-
-std::string outfile = "showsurf.out";
 
 int main(int argc, char** argv) 
 {
     // setup
-    randomer::seed(para.random_seed);
-    ioer::output_t out(outfile);
-    out.set_precision(6);
+    if (argparse(true, argc, argv) == false) {
+        return 0;
+    }
+
     const int Nele(static_cast<int>(para.Nbath / 2));
     const int Nhole(Nele + 1);
     potential_t potential(para.mass, para.omega, para.g, para.dG, para.gamma0);
-    hamiltonian_t hamiltonian(potential, para.bandwidth, Nele + Nhole, Nele + Nhole - 1, para.gamma0);
 
+    assert(not para.inttablefile.empty());
+    inttable_mgr_t inttable_mgr(para.inttablefile, potential);
+    inttable_mgr.load_inttable();
+    assert(inttable_mgr.kT == para.kT);
+    assert(inttable_mgr.gamma0 == para.gamma0);
 
-    double xmin(-10.0);
-    double xmax(30.0);
+    const double xmin(-10.0);
+    const double xmax(30.0);
     const int N(2000);
     const double dx((xmax - xmin) / N);
 
-    for (double x = xmin; x <= xmax; x += dx) {
-        hamiltonian.update_H(x);
-        hamiltonian.update_dc(x);
-        out.tabout( x, 
-                hamiltonian.cal_potential(x, 0), 
-                hamiltonian.cal_potential(x, 1), 
-                hamiltonian.cal_force(x, 0), 
-                hamiltonian.cal_force(x, 1),
-                hamiltonian.eva.at(1),
-                hamiltonian.eva.at(4),
-                hamiltonian.eva.at(9),
-                hamiltonian.dc.at(1 + 2 * hamiltonian.dim),
-                hamiltonian.dc.at(3 + 5 * hamiltonian.dim),
-                hamiltonian.dc.at(4 + 7 * hamiltonian.dim)
-              );
+    vector<double> xarr(N, 0.0);
+    vector<double> U0(N, 0.0), U1(N, 0.0);
+    vector<double> Ub0(N, 0.0), Ub1(N, 0.0);
+    xarr[0] = xmin;
+    U0[0] = potential.cal_potential(xmin, 0);
+    U1[0] = potential.cal_potential(xmin, 1);
+    Ub0[0] = U0[0];
+    Ub1[0] = U1[0];
+    for (int i(1); i < N; ++i) {
+        double x = xmin + i * dx;
+        xarr[i] = x;
+        U0[i] = U0[i - 1] - dx * potential.cal_force(x, 0);
+        U1[i] = U1[i - 1] - dx * potential.cal_force(x, 1);
+
+        double f(misc::fermi(potential.cal_h(x) / para.kT));
+        double dhdx(potential.cal_dhdx(x));
+        double fBCME(dhdx * f + inttable_mgr.retrieve("force", x));
+        Ub0[i] = Ub0[i - 1] - dx * (potential.cal_force(x, 0) + fBCME);
+        Ub1[i] = Ub1[i - 1] - dx * (potential.cal_force(x, 1) + fBCME);
     }
-    out.close();
+    ioer::h5file_t h5f(para.outfile, std::ios::out);
+    saveParatoh5(h5f);
+    h5f.create_dataset(
+            "x", xarr,
+            "U0", U0,
+            "U1", U1,
+            "Ub0", Ub0,
+            "Ub1", Ub1
+            );
+    h5f.close();
 
     return 0;
 }
